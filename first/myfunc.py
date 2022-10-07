775508b9af0a5e95ee7a39ff308bbd1f50629895
Николай <s@mail.com>
2022-10-07 14:29:25 +0300

    Второе сохранение через GitHub с ноутбука

--- first/myfunc.py
+++ first/myfunc.py
@@ -4,9 +4,13 @@ def sum(a,b):
 def dif(a, b):
     return a - b
 
+def mult(a, b):
+    return a * b
+
 if __name__ == '__main__':
     print(sum(2, 2))
     print(dif(5, 1))
+    print(mult(2, 5))
 print("Выход из программы")
 print("Выход из программы")
 print("Выход из программы")
