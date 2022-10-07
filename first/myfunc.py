24124c7e800335bc71914ef9b20013c5d34dfde9
SNickM <savoskinnikolay@yandex.ru>
2022-10-07 14:17:52 +0300

    Первое сохранения через GitHub

--- first/myfunc.py
+++ first/myfunc.py
@@ -1,9 +1,12 @@
 def sum(a,b):
     return a + b
 
+def dif(a, b):
+    return a - b
+
 if __name__ == '__main__':
     print(sum(2, 2))
-
+    print(dif(5, 1))
 print("Выход из программы")
 print("Выход из программы")
 print("Выход из программы")
